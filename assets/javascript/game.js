f507caaf66f68fac5a70a657e0186e84a27d444d
Jesse Schimmel <jhowardschimmel@gmail.com>
2019-06-18 00:02:15 -0400

    Removed Hello World! from the top of game.js added display-paragraph text onkeyup

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,18 @@
-document.write("Hello world!");
 document.addEventListener("keyup", function (event) {
     startGame();
 },
     { once: true }
 );
 
+document.onkeyup = function (event) {
+     const displayParagraph = document.getElementById("display-div");displayParagraph.innerText = "I HAVE CHANGED!"
+}
+
+
+
 function startGame() {
     const instructionsDiv = document.getElementById("instructions");
     const displayDiv = document.getElementById("display-div");
     instructionsDiv.style.display = "none";
     displayDiv.style.display = "block";
-    console.log("clicked!")
-
-
 }
