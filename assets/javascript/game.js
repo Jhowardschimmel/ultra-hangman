9f315fac616fce7628dce48301bb315ce18a982b
Jesse Schimmel <jhowardschimmel@gmail.com>
2019-06-18 00:40:41 -0400

    Added wordsArray to startGame function and returned wordsArray

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,9 +4,13 @@ document.addEventListener("keyup", function (event) {
     { once: true }
 );
 
+
+
 document.onkeyup = function (event) {
      const displayParagraph = document.getElementById("display-div");displayParagraph.innerText = "I HAVE CHANGED!"
 }
+
+
 
 
 
@@ -15,4 +19,8 @@ function startGame() {
     const displayDiv = document.getElementById("display-div");
     instructionsDiv.style.display = "none";
     displayDiv.style.display = "block";
+    const wordsArray = ["bulbasaur", "ivysaur", "venusaur"];
+    const computerGuess = wordsArray[Math.floor(Math.random()* wordsArray.length)];
+    return computerGuess;
 }
+
