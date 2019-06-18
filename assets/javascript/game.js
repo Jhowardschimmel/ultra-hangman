232819f3d923362ec982cafa68387cf2d8b92404
Jesse Schimmel <jhowardschimmel@gmail.com>
2019-06-17 21:58:52 -0400

    Event listener added for onkeyup calls startGame function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,8 @@
-document.write("Hello world!")
+document.write("Hello world!");
+document.onkeyup = function(event) {
+  startGame();
+};
+
+function startGame() {
+  document.write("Game has started!");
+}
