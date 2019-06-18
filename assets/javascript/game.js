271c8720f38ff4c47cf1bd338771147c808135b0
Jesse Schimmel <jhowardschimmel@gmail.com>
2019-06-18 13:51:56 -0400

    found two approaches to create empty place holders for computers guess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,13 +10,22 @@ document.addEventListener("keyup", function (event) {
 );
 
 function startGame() {
+    
     const instructionsDiv = document.getElementById("instructions");
     const displayDiv = document.getElementById("display-div");
     instructionsDiv.style.display = "none";
     displayDiv.style.display = "block";
     const wordsArray = ["bulbasaur", "ivysaur", "venusaur"];
     const computerGuess = wordsArray[Math.floor(Math.random() * wordsArray.length)];
-    const displayParagraph = document.getElementById("display-div"); displayParagraph.innerText = computerGuess;
+    const displayParagraph = document.getElementById("display-paragraph");
+    const fromguessArray = Array.from(computerGuess, element => "_");
+    console.log(fromguessArray);
+    const mapguessArray = computerGuess.split("").map(element => "_")
+    console.log(mapguessArray)
+
+
+
+    displayParagraph.innerText = computerGuess;
 }
 
 
