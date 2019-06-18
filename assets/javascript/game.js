3a9f6ca960dd41d94693831d10cba36e4cca02e1
Jesse Schimmel <jhowardschimmel@gmail.com>
2019-06-18 02:12:04 -0400

    Deleted 2nd listener made changes to DOM in first listener and put all logic in startGame

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,13 @@
 document.addEventListener("keyup", function (event) {
+    const instructionsDiv = document.getElementById("instructions");
+    const displayDiv = document.getElementById("display-div");
+    instructionsDiv.style.display = "none";
+    displayDiv.style.display = "block";
     startGame();
+
 },
     { once: true }
 );
-
-
-
-document.onkeyup = function (event) {
-     const displayParagraph = document.getElementById("display-div");displayParagraph.innerText = "I HAVE CHANGED!"
-}
-
-
-
-
 
 function startGame() {
     const instructionsDiv = document.getElementById("instructions");
@@ -20,7 +15,20 @@ function startGame() {
     instructionsDiv.style.display = "none";
     displayDiv.style.display = "block";
     const wordsArray = ["bulbasaur", "ivysaur", "venusaur"];
-    const computerGuess = wordsArray[Math.floor(Math.random()* wordsArray.length)];
-    return computerGuess;
+    const computerGuess = wordsArray[Math.floor(Math.random() * wordsArray.length)];
+    const displayParagraph = document.getElementById("display-div"); displayParagraph.innerText = computerGuess;
 }
+
+
+
+
+document.onkeyup = function (event) {
+    console.log(event.key)
+}
+
+
+
+
+
+
 
